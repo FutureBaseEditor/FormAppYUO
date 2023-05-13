5b758969a0c4c9e05023afd5ded4771d2918d6d7
sedat.akkus <sedat.akkus@base86.net>
2023-05-13 15:43:51 +0300

    feat: add good cheap fasrt page

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -50,6 +50,7 @@
             // 
             this.textBox2.Location = new System.Drawing.Point(207, 186);
             this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
             this.textBox2.Size = new System.Drawing.Size(125, 27);
             this.textBox2.TabIndex = 1;
             // 
---
+++ Form2.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormAppYUO
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.BackColor == Color.YellowGreen)
+                button1.BackColor = Color.Brown;
+            else if (button1.BackColor == Color.Brown)
+            {
+                button1.BackColor = Color.YellowGreen;
+                if (button2.BackColor == Color.YellowGreen)
+                    button3.BackColor = Color.Brown;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (button2.BackColor == Color.YellowGreen)
+                button2.BackColor = Color.Brown;
+            else if (button2.BackColor == Color.Brown)
+            {
+                button2.BackColor = Color.YellowGreen;
+                if (button3.BackColor == Color.YellowGreen)
+                    button1.BackColor = Color.Brown;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (button3.BackColor == Color.YellowGreen)
+                button3.BackColor = Color.Brown;
+            else if (button3.BackColor == Color.Brown)
+            {
+                button3.BackColor = Color.YellowGreen;
+                if (button1.BackColor == Color.YellowGreen)
+                    button2.BackColor = Color.Brown;
+            }
+        }
+    }
+}
